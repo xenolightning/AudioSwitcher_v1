c434e7410a49aad01a301ba5819b253e8062d163
Sean Chapman <sean.b.chapman@gmail.com>
2015-01-28 17:47:02 +1300

    Updated copyright year to 2015

--- FortyOne.AudioSwitcher/Properties/AssemblyInfo.cs
+++ FortyOne.AudioSwitcher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Forty One Ltd.")]
 [assembly: AssemblyProduct("Audio Switcher")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
