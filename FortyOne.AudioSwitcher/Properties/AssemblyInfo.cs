b8e42f0434e67115b55308d0ef9325aa8743585f
xenolightning <sean.b.chapman@gmail.com>
2015-06-23 17:29:43 +1200

    Version bump

--- FortyOne.AudioSwitcher/Properties/AssemblyInfo.cs
+++ FortyOne.AudioSwitcher/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.6.3.1")]
-[assembly: AssemblyFileVersion("1.6.3.1")]
-[assembly: AssemblyInformationalVersion("1.6.3.1")]
+[assembly: AssemblyVersion("1.6.6.0")]
+[assembly: AssemblyFileVersion("1.6.6.0")]
+[assembly: AssemblyInformationalVersion("1.6.6.0")]
 [assembly: NeutralResourcesLanguage("")]
