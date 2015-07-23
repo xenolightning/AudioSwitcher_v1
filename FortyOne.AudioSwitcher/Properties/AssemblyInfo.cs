5ef289b5a2cad600069d35b3c121bc307b1a5705
xenolightning <sean.b.chapman@gmail.com>
2015-07-23 23:06:57 +1200

    1.6.6 Release
    Fixed Icons

--- FortyOne.AudioSwitcher/Properties/AssemblyInfo.cs
+++ FortyOne.AudioSwitcher/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.6.6.83")]
-[assembly: AssemblyFileVersion("1.6.6.83")]
-[assembly: AssemblyInformationalVersion("1.6.6.83")]
+[assembly: AssemblyVersion("1.6.6.84")]
+[assembly: AssemblyFileVersion("1.6.6.84")]
+[assembly: AssemblyInformationalVersion("1.6.6.84")]
 [assembly: NeutralResourcesLanguage("")]
