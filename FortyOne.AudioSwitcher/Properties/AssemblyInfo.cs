c291d0414eafc6c776ffb1db933881987f10bf9a
xenolightning <sean.b.chapman@gmail.com>
2015-12-11 21:36:12 +1300

    1.6.9.102 Release

--- FortyOne.AudioSwitcher/Properties/AssemblyInfo.cs
+++ FortyOne.AudioSwitcher/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.6.8.99")]
-[assembly: AssemblyFileVersion("1.6.8.99")]
-[assembly: AssemblyInformationalVersion("1.6.8.99")]
+[assembly: AssemblyVersion("1.6.9.102")]
+[assembly: AssemblyFileVersion("1.6.9.102")]
+[assembly: AssemblyInformationalVersion("1.6.9.102")]
 [assembly: NeutralResourcesLanguage("")]
