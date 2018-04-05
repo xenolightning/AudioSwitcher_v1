da95b8837316069e4464ad2c6a79ebc84331f06c
xenolightning <sean.b.chapman@gmail.com>
2018-04-05 22:10:33 +1200

    Version bump for release

--- FortyOne.AudioSwitcher/Properties/AssemblyInfo.cs
+++ FortyOne.AudioSwitcher/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Forty One Ltd.")]
 [assembly: AssemblyProduct("Audio Switcher")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.7.1.118")]
-[assembly: AssemblyFileVersion("1.7.1.118")]
-[assembly: AssemblyInformationalVersion("1.7.1.118")]
+[assembly: AssemblyVersion("1.8.0.142")]
+[assembly: AssemblyFileVersion("1.8.0.142")]
+[assembly: AssemblyInformationalVersion("1.8.0.142")]
 [assembly: NeutralResourcesLanguage("")]
