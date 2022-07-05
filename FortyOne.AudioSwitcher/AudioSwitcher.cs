ee6c00c9be9e02d13d00e81606544d0b72eeddc1
ziroc <37216225+ziroc@users.noreply.github.com>
2022-07-05 10:10:59 +0300

    Adding a checkbox for controlling the the showing of the window on double click

--- FortyOne.AudioSwitcher/AudioSwitcher.cs
+++ FortyOne.AudioSwitcher/AudioSwitcher.cs
@@ -765,6 +765,7 @@ namespace FortyOne.AudioSwitcher
             chkAutoStartWithWindows.Checked = Program.Settings.AutoStartWithWindows;
             chkDisableHotKeys.Checked = Program.Settings.DisableHotKeys;
             chkQuickSwitch.Checked = Program.Settings.EnableQuickSwitch;
+            chkDisableDblClick.Checked = Program.Settings.DisableDoubleClick;
             chkDualSwitchMode.Checked = Program.Settings.DualSwitchMode;
             chkNotifyUpdates.Checked = Program.Settings.UpdateNotificationsEnabled;
 
@@ -1317,9 +1318,12 @@ namespace FortyOne.AudioSwitcher
 
         private void notifyIcon1_DoubleClick(object sender, EventArgs e)
         {
-            Show();
-            BringToFront();
-            SetForegroundWindow(Handle);
+            if (!Program.Settings.DisableDoubleClick)
+            {
+                Show();
+                BringToFront();
+                SetForegroundWindow(Handle);
+            }
         }
 
         private void Form1_Activated(object sender, EventArgs e)
@@ -1363,6 +1367,10 @@ namespace FortyOne.AudioSwitcher
         private void chkQuickSwitch_CheckedChanged(object sender, EventArgs e)
         {
             Program.Settings.EnableQuickSwitch = chkQuickSwitch.Checked;
+        }
+        private void chkDisableDblClick_CheckedChanged(object sender, EventArgs e)
+        {
+            Program.Settings.DisableDoubleClick = chkDisableDblClick.Checked;
         }
 
         private void chkDualSwitchMode_CheckedChanged(object sender, EventArgs e)
